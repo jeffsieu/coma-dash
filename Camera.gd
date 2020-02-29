92fc345a1b7599c9a16061ebd30c5b47487e4e30
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-02-29 10:03:36 +0800

    Change camera to follow y position of player too
    Because when changing levels and updating the player position to the new
    level player position, it may not exactly touch the floor and will fall
    down a bit due to gravity. But even though the player falls, the camera
    doesn't, which means it will zoom out a bit permanently.

--- Camera.gd
+++ Camera.gd
@@ -5,18 +5,19 @@ var _initialized := false
 var _velocity: Vector3
 const _BASE_ACCELERATION = 80
 const _FRICTION = 0.9
+const _CAMERA_HEIGHT = 25
 const _THRESHOLD_DISTANCE = 2 # Maximum distance from the player at which the camera won't move
 
 func _physics_process(delta: float) -> void:
 	if not _initialized:
-		transform.origin.x = _player.transform.origin.x
-		transform.origin.z = _player.transform.origin.z
+		transform.origin = _player.transform.origin
 		_initialized = true
 		return
-	var player_origin = Vector3(_player.transform.origin.x, 0, _player.transform.origin.z)
-	var camera_origin = Vector3(transform.origin.x, 0, transform.origin.z)
-	var direction = camera_origin.direction_to(player_origin)
-	var distance_to_player = camera_origin.distance_to(player_origin) - _THRESHOLD_DISTANCE
+
+	var player_origin := _player.transform.origin
+	var target_origin = Vector3(player_origin.x, player_origin.y + _CAMERA_HEIGHT, player_origin.z)
+	var direction = transform.origin.direction_to(target_origin)
+	var distance_to_player = transform.origin.distance_to(target_origin) - _THRESHOLD_DISTANCE
 	distance_to_player = max(distance_to_player, 0)
 
 	var acceleration = _BASE_ACCELERATION * distance_to_player * direction
