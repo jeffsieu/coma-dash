ac128825e5b6e9a1bbda2c9ea30be4d257c3292a
Jeff Sieu <jeffsy00@gmail.com>
2020-03-08 23:05:13 +0800

    Use simplex noise for camera shake

--- Camera/Camera.gd
+++ Camera/Camera.gd
@@ -6,18 +6,25 @@ var _initialized := false
 var _velocity: Vector3
 var _actual_position: Vector3
 var _trauma := 0.0
+var _shake_delta := 0.0
 
 const _BASE_ACCELERATION = 80
 const _FRICTION = 0.9
 const _CAMERA_HEIGHT = 25
 const _THRESHOLD_DISTANCE = 2 # Maximum distance from the player at which the camera won't move
 
-const _MAX_TRAUMA = 1.1;
-const _TRAUMA_FALL = 0.1;
-const _ROTATION_SHAKE_SCALE = 1.2
-const _X_SHAKE_SCALE = 1.2
-const _Z_SHAKE_SCALE = 1.2
+const _MAX_TRAUMA = 1;
+const _TRAUMA_FALL = 0.05;
+const _ROTATION_SHAKE_SCALE = 5
+const _X_SHAKE_SCALE = 1
+const _Z_SHAKE_SCALE = 1
 const _Y_ROTATION_BASE = -90
+const _NOISE_PERIOD = 0.5
+
+onready var noise := OpenSimplexNoise.new()
+
+func _ready() -> void:
+	noise.set_period(_NOISE_PERIOD)
 
 func _physics_process(delta: float) -> void:
 	if not _initialized:
@@ -39,18 +46,21 @@ func _physics_process(delta: float) -> void:
 	_velocity *= _FRICTION
 	_actual_position += _velocity * delta
 
-	_screenshake()
+	_screen_shake_process(delta)
 
-func _screenshake() -> void:
+func _screen_shake_process(delta: float) -> void:
+	_trauma = clamp(_trauma - _TRAUMA_FALL, 0, _MAX_TRAUMA)
 	var shake := pow(_trauma, 2)
 	var x := _actual_position.x
 	var z := _actual_position.z
 	transform.origin = _actual_position
-	transform.origin.x += _X_SHAKE_SCALE * shake * (randf() * 2 - 1)
-	transform.origin.z += _Z_SHAKE_SCALE * shake * (randf() * 2 - 1)
-	rotation_degrees.y = _Y_ROTATION_BASE + _ROTATION_SHAKE_SCALE * shake * (randf() * 2 - 1)
+	transform.origin.x += _X_SHAKE_SCALE * shake * noise.get_noise_2d(0, _shake_delta)
+	transform.origin.z += _Z_SHAKE_SCALE * shake * noise.get_noise_2d(1, _shake_delta)
+	rotation_degrees.y = _Y_ROTATION_BASE + _ROTATION_SHAKE_SCALE * shake * noise.get_noise_2d(2, _shake_delta)
 
-	_trauma = max(0, _trauma - _TRAUMA_FALL)
+	_shake_delta += delta
+	if _trauma == 0:
+		_shake_delta = 0
 
-func apply_damage_trauma(trauma: float) -> void:
-	_trauma = min(max(_trauma, trauma), _MAX_TRAUMA)
+func apply_damage_trauma(effectiveness: float) -> void:
+	_trauma += effectiveness
