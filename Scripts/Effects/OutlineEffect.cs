202eea76a31064f4ac46360544580880dce00fdb
Owen Leong <owenl131@gmail.com>
2020-05-06 23:29:40 +0800

    Fix whitespace formatting

--- Scripts/Effects/OutlineEffect.cs
+++ Scripts/Effects/OutlineEffect.cs
@@ -21,11 +21,4 @@ public class OutlineEffect : MeshInstance
         material.SetShaderParam("pixels_y", size.y);
     }
 
-    
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
