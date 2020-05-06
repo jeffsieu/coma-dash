844c806e6a6645b31fd3b0e96e9f1c60b61b5957
Owen Leong <owenl131@gmail.com>
2020-05-06 23:23:11 +0800

    Fix previous bug in outline shader, add anti-aliasing

---
+++ Scripts/Effects/OutlineEffect.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class OutlineEffect : MeshInstance
+{
+    ShaderMaterial material;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        material = Mesh.SurfaceGetMaterial(0) as ShaderMaterial;
+        GetTree().Root.Connect("size_changed", this, "ScreenResized");
+        ScreenResized();
+    }
+
+    public void ScreenResized()
+    {
+        Vector2 size = OS.WindowSize;
+        GD.Print(size);
+        material.SetShaderParam("pixels_x", size.x);
+        material.SetShaderParam("pixels_y", size.y);
+    }
+
+    
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
