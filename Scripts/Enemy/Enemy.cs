b41bee5954374570129d58cbed75d86f0855a1fa
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-05-07 23:07:53 +0800

    AngstyEnemy: Enemy that charges and knocks back the player
    AngstyEnemy provides an example on how we can move towards the player and rotate to face the player using the MoveToPlayer and RotateToPlayer methods exposed by the base Enemy class.
    
    These functions are left to the children class to call themselves so that they have full control over their actions.
    
    AngstyEnemy also provides an implementation for charging towards the player to attack it. When it hits the player, the player will be knocked back by a factor of the AngstyEnemy's velocity.
    
    As the saying goes, united we stand. When all enemies charge towards the player, the player will fly backwards by a large amount.
    
    The AngstyEnemy mesh is changed to a CSGBox so that the rotation can be seen.
    
    Additionally, add gravity to player otherwise the player might fly out
    of the game when hit.
    
    Still doesn't have path finding towards the player, because the AStar class needs us to initialize it with some information about the room. So will do that after room gen is done.

--- Scripts/Enemy/Enemy.cs
+++ Scripts/Enemy/Enemy.cs
@@ -3,46 +3,52 @@ using Godot;
 public class Enemy : HealthEntity
 {
     public Vector3 Velocity;
-    private readonly float gravity = 4.85f;
-    private SpatialMaterial material;
+    protected readonly float gravity = 4.85f;
+
+    protected Player player;
+    protected float acceleration = 5f;
+    protected float angularSpeed = 3f;
 
     public Enemy()
     {
-        MaxHealth = 100;
+        Velocity = Vector3.Zero;
     }
 
     public override void _Ready()
     {
         base._Ready();
-        material = new SpatialMaterial()
-        {
-            AlbedoColor = Colors.White,
-            FlagsTransparent = false
-        };
-
-        MeshInstance cylinder = GetNode<MeshInstance>("MeshInstance");
-        cylinder.SetSurfaceMaterial(0, material);
+        player = GetTree().Root.GetNode("Level").GetNodeOrNull<Player>("Player");
     }
 
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
-        Velocity.x *= 0.9f;
-        Velocity.y -= gravity;
-        Velocity.z *= 0.9f;
-        Velocity = MoveAndSlide(Velocity);
-        Color baseColor = Colors.Red.LinearInterpolate(Colors.Blue, GetPercentLeft<MarkStatus>());
-        Color healthColor = baseColor.LinearInterpolate(Colors.White, 1 - Health / 100);
-        material.AlbedoColor = healthColor;
+    }
+
+    // Currently it is just the player's position since there are no obstacles
+    // Once the level generator is done, we can use AStar, and the return value will be the next point to move to
+    // in pathfinding to player
+    protected Vector3 NextPointInPath()
+    {
+        return player.GlobalTransform.origin;
+    }
+
+    protected void MoveToPlayer(float delta)
+    {
+        Vector3 moveDirection = NextPointInPath() - GlobalTransform.origin;
+        Velocity += acceleration * moveDirection.Normalized() * delta;
+    }
+
+    protected void RotateToPlayer(float delta)
+    {
+        Vector3 directionToPlayer = NextPointInPath() - GlobalTransform.origin;
+        float angle = Mathf.Pi - GlobalTransform.basis.z.AngleTo(directionToPlayer);
+        float angleSign = -Mathf.Sign(GlobalTransform.basis.x.Dot(directionToPlayer));
+        float rotationAngle = Mathf.Min(angularSpeed * delta, angle) * angleSign;
+        Rotate(Vector3.Up, rotationAngle);
     }
 
     protected override void Die()
     {
-        material.FlagsTransparent = true;
-        Tween tween = new Tween();
-        AddChild(tween);
-        tween.InterpolateProperty(material, "albedo_color:a", 0.5f, 0, 1.0f);
-        tween.InterpolateCallback(this, 1.0f, "queue_free");
-        tween.Start();
     }
 }
