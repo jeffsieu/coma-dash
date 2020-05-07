ded4857b122f3897c45a731ec59d129376185d04
Owen Leong <owenl131@gmail.com>
2020-05-07 16:25:23 +0800

    Outline shader enhancement (#116)
    * Fix previous bug in outline shader, add anti-aliasing
    
    * Fix whitespace formatting
    
    * Improve sampling
    
    * Merge from dev

---
+++ Scripts/Effects/OutlineEffect.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+public class OutlineEffect : MeshInstance
+{
+    ShaderMaterial material;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        material = Mesh.SurfaceGetMaterial(0) as ShaderMaterial;
+        GetTree().Root.Connect("size_changed", this, "ScreenResized");
+        ScreenResized();
+    }
+
+    public void ScreenResized()
+    {
+        Vector2 size = OS.WindowSize;
+        material.SetShaderParam("pixels_x", size.x);
+        material.SetShaderParam("pixels_y", size.y);
+    }
+}
