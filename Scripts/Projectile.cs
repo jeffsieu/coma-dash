8e42b1d16508b1ceb4777a114b8aa7bb2a65be8c
Owen Leong <owenl131@gmail.com>
2020-05-09 18:37:23 +0800

    Fix whitespace

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -57,7 +57,7 @@ public class Projectile : Spatial
 
         // Destroy when the front edge reaches the end point
         // if (distanceTravelled > range - projectileLength)
-            // QueueFree();
+        // QueueFree();
 
         // Translation += displacementToTravel;
     }
