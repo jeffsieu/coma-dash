9f357ae4e607a3884cba3ed2606dbed7ac697cf6
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-06-11 13:50:26 +0800

    Room: Split out setting shader params into one method

--- Scripts/Generator/Room.cs
+++ Scripts/Generator/Room.cs
@@ -3,34 +3,16 @@ using Godot;
 
 public class Room : StaticBody
 {
+    private CSGPolygon floorMesh;
+
     public Room(Vector2[][] polygon, int unitSize, Material material)
     {
         RotationDegrees = new Vector3(90, 0, 0);
         Scale = unitSize * Vector3.One;
-        CSGPolygon floorMesh = new CSGPolygon
+        floorMesh = new CSGPolygon
         {
             Polygon = polygon[0]
         };
-
-        float minX = polygon[0][0].x, maxX = polygon[0][0].x;
-        float minY = polygon[0][0].y, maxY = polygon[0][0].y;
-        foreach (Vector2 point in polygon[0])
-        {
-            minX = Mathf.Min(point.x, minX);
-            maxX = Mathf.Max(point.x, maxX);
-            minY = Mathf.Min(point.y, minY);
-            maxY = Mathf.Max(point.y, maxY);
-        }
-
-        float width = maxX - minX;
-        float height = maxY - minY;
-
-        // make a duplicate of the shader so that can customize the `size` uniform
-        // might want to see if this costs anything? I don't think so though
-        // 5 lines of shader code shouldn't cost anything
-        ShaderMaterial dupMaterial = (ShaderMaterial)material.Duplicate();
-        dupMaterial.SetShaderParam("size", new Vector2(width, height));
-        floorMesh.Material = dupMaterial;
 
         for (int i = 1; i < polygon.Length; ++i)
         {
@@ -47,7 +29,32 @@ public class Room : StaticBody
             });
         }
 
+        SetFloorShaderParams(polygon[0], (ShaderMaterial) material);
+
         AddChild(floorMesh);
         Translation = new Vector3(0, -unitSize, 0);
+    }
+
+    private void SetFloorShaderParams(Vector2[] polygon, ShaderMaterial material)
+    {
+        float minX = polygon[0].x, maxX = polygon[0].x;
+        float minY = polygon[0].y, maxY = polygon[0].y;
+        foreach (Vector2 point in polygon)
+        {
+            minX = Mathf.Min(point.x, minX);
+            maxX = Mathf.Max(point.x, maxX);
+            minY = Mathf.Min(point.y, minY);
+            maxY = Mathf.Max(point.y, maxY);
+        }
+
+        float width = maxX - minX;
+        float height = maxY - minY;
+
+        // make a duplicate of the shader so that can customize the `size` uniform
+        // might want to see if this costs anything? I don't think so though
+        // 5 lines of shader code shouldn't cost anything
+        ShaderMaterial dupMaterial = (ShaderMaterial) material.Duplicate();
+        dupMaterial.SetShaderParam("size", new Vector2(width, height));
+        floorMesh.Material = dupMaterial;
     }
 }
